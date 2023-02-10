1458dc3c502e1103cba6e2952ed6f4b67b5ed105
njokichristine <christinenjoki6900@gmail.com>
2023-02-10 18:04:05 +0300

    completed

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+# define the class Dog
+class Dog
+
+  # set initialize method for the dog breed and name
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+
+end
